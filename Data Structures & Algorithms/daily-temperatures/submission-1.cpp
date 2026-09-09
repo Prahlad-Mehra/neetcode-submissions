428eba0c64e5458a2f116e1c55b8642bbba6eaee
Prahlad Mehra <prahladmehra98@gmail.com>
2026-09-10 01:46:08 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,33 @@
+struct Node{
+    int val;
+    int idx;
+};
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<Node> st;
+        vector<int> result(n, 0);
+
+        for(int i=n-1; i>=0; i--){
+            int curr_temp = temperatures[i];
+            if(st.empty()){
+                st.push({curr_temp,i});
+            }
+            else{
+                while(!st.empty() && st.top().val<=curr_temp){
+                    st.pop();
+                }
+                if(st.empty()){
+                    st.push({curr_temp,i});
+                    continue;
+                }
+                result[i] = st.top().idx-i;
+                st.push({curr_temp,i});
+            }
+        }
+
+        return result;
+    }
+};
