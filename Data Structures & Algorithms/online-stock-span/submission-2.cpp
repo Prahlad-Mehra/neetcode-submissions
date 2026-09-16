02d0db9d245ffbbe35f36454f40b40fa7250f530
Prahlad Mehra <prahladmehra98@gmail.com>
2026-09-16 20:47:05 +0530

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.cpp
@@ -0,0 +1,29 @@
+class StockSpanner {
+private:
+    stack<pair<int,int>> st; // {idx, value}
+    int count = 0;
+public:
+    StockSpanner() {
+        // do nothing still win 
+    }
+    
+    int next(int price) {
+        if(st.empty()){
+            st.push({count++, price});
+            return 1;
+        }
+        while(!st.empty() && st.top().second <= price)
+            st.pop();
+        
+        int prevIdx = st.empty() ? -1 : st.top().first;
+        st.push({count++, price});
+
+        return count-prevIdx-1;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
